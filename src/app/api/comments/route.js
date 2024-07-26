53e56af3732f1c98aa851cf6f6b53ef2316bedd8
Ajay kumar <akjajay9315@gmail.com>
2024-07-26 16:50:48 +0530

    route

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,63 +1,3 @@
-// import { getAuthSession } from "@/utils/auth";
-// import prisma from "@/utils/connect";
-// import { NextResponse } from "next/server";
-
-// // Handle both GET and POST requests
-// export const handler = async (req) => {
-//   if (req.method === "GET") {
-//     // GET ALL COMMENTS OF A POST
-//     const { searchParams } = new URL(req.url);
-//     const postSlug = searchParams.get("postSlug");
-
-//     try {
-//       const comments = await prisma.comment.findMany({
-//         where: {
-//           postSlug: postSlug,
-//         },
-//         include: { user: true },
-//       });
-
-//       return new NextResponse(JSON.stringify(comments), { status: 200 });
-//     } catch (err) {
-//       console.log(err);
-//       return new NextResponse(
-//         JSON.stringify({ message: "Something went wrong!" }),
-//         { status: 500 }
-//       );
-//     }
-//   } else if (req.method === "POST") {
-//     // CREATE A COMMENT
-//     const session = await getAuthSession();
-
-//     if (!session) {
-//       return new NextResponse(
-//         JSON.stringify({ message: "Not Authenticated!" }),
-//         { status: 401 }
-//       );
-//     }
-
-//     try {
-//       const body = await req.json();
-//       const comment = await prisma.comment.create({
-//         data: { ...body, userEmail: session.user.email },
-//       });
-
-//       return new NextResponse(JSON.stringify(comment), { status: 200 });
-//     } catch (err) {
-//       console.log(err);
-//       return new NextResponse(
-//         JSON.stringify({ message: "Something went wrong!" }),
-//         { status: 500 }
-//       );
-//     }
-//   } else {
-//     return new NextResponse(
-//       JSON.stringify({ message: "Method not allowed" }),
-//       { status: 405 }
-//     );
-//   }
-// };
-
 import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
@@ -79,7 +19,7 @@ export const handler = async (req) => {
 
       return new NextResponse(JSON.stringify(comments), { status: 200 });
     } catch (err) {
-      console.error(err);
+      console.log(err);
       return new NextResponse(
         JSON.stringify({ message: "Something went wrong!" }),
         { status: 500 }
@@ -104,15 +44,16 @@ export const handler = async (req) => {
 
       return new NextResponse(JSON.stringify(comment), { status: 200 });
     } catch (err) {
-      console.error(err);
+      console.log(err);
       return new NextResponse(
         JSON.stringify({ message: "Something went wrong!" }),
         { status: 500 }
       );
     }
   } else {
-    return new NextResponse(JSON.stringify({ message: "Method not allowed" }), {
-      status: 405,
-    });
+    return new NextResponse(
+      JSON.stringify({ message: "Method not allowed" }),
+      { status: 405 }
+    );
   }
 };
