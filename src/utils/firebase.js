bdffe4ebe423a89ab92f3930b052cf1b64edae89
Ajay kumar <akjajay9315@gmail.com>
2024-07-26 12:58:05 +0530

    FIREBASE

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,8 +1,31 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDnsZxdHSZqWN0SmjT_qvTs755I3dp_Wec",
+//   authDomain: "ojiblogs.firebaseapp.com",
+//   projectId: "ojiblogs",
+//   storageBucket: "ojiblogs.appspot.com",
+//   messagingSenderId: "124081812780",
+//   appId: "1:124081812780:web:cedf69fc22ded0f7a6a889",
+//   measurementId: "G-0M5XGC0WTD",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// export { app }; // Export the `app` variable
+
+
+// firebaseConfig.js
+// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,8 +40,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export { app }; // Export the `app` variable
+// Initialize Analytics (optional, remove if not needed)
+const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
 
+export { app, analytics };
 
