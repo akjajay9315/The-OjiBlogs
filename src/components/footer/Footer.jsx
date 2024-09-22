0d73236f28c904141c4cb6df0c8721b1fdd6d0c0
Ajay kumar <akjajay9315@gmail.com>
2024-09-22 22:03:42 +0530

    pre views

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
-  const [visitCount, setVisitCount] = useState(200);
+  const [visitCount, setVisitCount] = useState(300);
 
   useEffect(() => {
     const sessionKey = "hasVisited";
@@ -17,7 +17,7 @@ const Footer = () => {
     if (!hasVisited) {
       // If not visited, increment the count
       const count = localStorage.getItem(countKey);
-      const newCount = count ? parseInt(count) + 1 : 1;
+      const newCount = count ? parseInt(count) + 1 : 301;
 
       localStorage.setItem(countKey, newCount);
       setVisitCount(newCount);
